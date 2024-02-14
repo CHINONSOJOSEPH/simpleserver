5dc5456f41157fa34b703e8e1df98d6c89eae9a0
Ojeh Joseph Chinonso <chinonsojoseph8182@gmail.com>
2024-02-14 04:13:53 +0100

    simple Server

---
+++ assignment1.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+const HOSTNAME = "localhost"
+
+const PORT = 8900;
+
+const server = http.createServer(requesthandler);
+
+function requesthandler(req, res){
+   res.end("My Name is Ojeh Joseph")
+
+};
+
+server.listen(PORT,HOSTNAME, () => {
+    console.log(`server is listening at http://${HOSTNAME}:${PORT}`);
+});
